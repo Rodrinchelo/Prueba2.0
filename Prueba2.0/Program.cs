de6b68c92cc9fc45916e723f5602656812f10fbe
Rodrigo alonso almeyda ibarra <rodrigo.almeyda@unmsm.edu.pe>
2020-07-25 00:12:48 -0500

    Agregar archivos de proyecto.

---
+++ Prueba2.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prueba2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
